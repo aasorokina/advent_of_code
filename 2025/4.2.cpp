be3ebe3966f83d4b792fb59ba2229dec2179db3a
buggkell <buggkell@student.21-school.ru>
2025-12-07 23:37:13 +0300

    chore: add 4.2 solution

---
+++ 2025/4.2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+const char empty = '.';
+const char paper = '@';
+
+int main() {
+  std::string line;
+  std::vector<std::string> grid;
+  unsigned long long result = 0;
+
+  while (std::getline(std::cin, line)) grid.push_back(line);
+
+  bool deleted = true;
+  while (deleted) {
+    deleted = false;
+    for (int i = 0, grid_size = grid.size(); i < grid_size; ++i) {
+      int max_i = grid_size - 1;
+
+      for (int j = 0, line_size = grid[i].size(); j < line_size; ++j) {
+        if (grid[i][j] != paper) continue;
+
+        int max_j = line_size - 1;
+        int count = ((i > 0) && (grid[i - 1][j] == paper)) +
+                    ((j > 0) && (grid[i][j - 1] == paper)) +
+                    ((i < max_i) && (grid[i + 1][j] == paper)) +
+                    ((j < max_j) && (grid[i][j + 1] == paper)) +
+                    ((i > 0 && j > 0) && (grid[i - 1][j - 1] == paper)) +
+                    ((i < max_i && j > 0) && (grid[i + 1][j - 1] == paper)) +
+                    ((i > 0 && j < max_j) && (grid[i - 1][j + 1] == paper)) +
+                    ((i < max_i && j < max_j) && (grid[i + 1][j + 1] == paper));
+
+        if (count < 4) {
+          result++;
+          deleted = true;
+          grid[i][j] = empty;
+        }
+      }
+    }
+  };
+
+  std::cout << result << '\n';
+}
